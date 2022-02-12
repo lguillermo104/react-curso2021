0a6c07192b4354085361323320653a77e70b332e
Vicente <lguillermo_104@hotmail.com>
2022-02-12 06:49:44 -0500

    Ejemplo de propiedades

--- src/App.js
+++ src/App.js
@@ -1,27 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
 import Componente from './components/Componente';
+import Propiedades from "./components/Propiedades";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <section>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </section>
+
+        <section>
+          <Componente msg="Bienvenido a los componentes"></Componente>
+          <hr />
+          <Propiedades 
+            cadena="Esto es una cadena de texto" 
+            numero= {19}
+            booleano={false}
+            arreglo = {[1, 2, 3]} 
+            objeto = {{nombre: "Vicente", correo: "lguillermo@hotmail.com"}}
+            funcion = { (num) => num * num }
+            elementoReact = {<i>Esto es un elemento React</i>}
+            componenteReact = {<Componente msg="Soy un componente pasado como props" />}
+          />
+        </section>
+
       </header>
-      <section>
-        <Componente msg="Bienvenido a los componentes"></Componente>
-      </section>
+    
     </div>
   );
 }
